4d044419e29f4e88a1c74fb18687fdf477641281
dkohlruss <dkohlruss@gmail.com>
2017-03-13 09:10:16 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  let ipaddress = req.connection.remoteAddress;
+  let rawBrowser = req.headers['user-agent'];
+  let rawLanguage = req.headers['accept-language'];
+
+  let browser = browserify(rawBrowser);
+  let language = langify(rawLanguage);
+
+  function browserify(raw) {
+    let openBracket = raw.indexOf('(') + 1;
+    let closeBracket = raw.indexOf(')');
+
+    return raw.substring(openBracket, closeBracket);
+  };
+
+  function langify(raw) {
+    let comma = raw.indexOf(',');
+
+    return raw.substring(0, comma);
+  }
+
+
+  res.send({
+    ipaddress,
+    browser,
+    language
+  })
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server started');
+})
